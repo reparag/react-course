5007ab46424b605969ef364b96e447a7c4d63d70
Parag Patil <parag.p@refurbedge.com>
2026-01-21 19:09:23 +0530

    disecting react

---
+++ 01basicreact/src/Youtube.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Youtube() {
+  return (
+    <div>
+      <h1>Welcome To Youtube</h1>
+    </div>
+  );
+}
---
+++ 01basicvite/src/Welcome.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Welcome() {
+  return (
+    <div>
+      <h1>Welcome To React</h1>
+    </div>
+  );
+}
